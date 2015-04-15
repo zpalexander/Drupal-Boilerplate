e4863b4b0646849fe64607f6b636f0e0ed0115a8
Zach Alexander <zach@elephantventures.com>
2015-04-15 14:33:36 +0800

    Update example drush aliases file

--- drush/aliases/example.aliases.drushrc.php
+++ drush/aliases/example.aliases.drushrc.php
@@ -5,55 +5,64 @@
  * Default drush aliases.drushrc.php file.
  */
 
-/**
- * These are the default configuration so that
- * everyone can just overwrite the different settings.
- */
-
-$aliases['dev'] = array(
- 'uri' => 'example.lan',
- 'root' => str_replace('drush/aliases', 'docroot', dirname(__FILE__)),
-);
-
-$aliases['stage'] = array(
-  'uri' => 'stage.example.com',
-  'root' => '/var/www/stage.example.com/docroot',
-  'remote-host' => 'example.com',
-  'remote-user' => 'user',
-);
-
-$aliases['live'] = array(
-  'uri' => 'example.com',
-  'root' => '/var/www/stage.example.com/docroot',
-  'remote-host' => 'example.com',
-  'remote-user' => 'user',
-);
-
-
-
-/**
- * A sample Acquia alias
- */
-
+// Grab the most recent major version of Drush
 if (!isset($drush_major_version)) {
   $drush_version_components = explode('.', DRUSH_VERSION);
   $drush_major_version = $drush_version_components[0];
 }
 
-// Site urban, environment dev
+/**
+ * A sample local alias
+ */
+$aliases['local'] = array(
+  'parent' => '@parent',
+  'site' => 'example',
+  'env' => 'local',
+  'uri' => 'example.dev',
+  'root' => '/home/vagrant/sites/example',
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp'
+  ),
+);
+
+
+/**
+ * A sample Pantheon alias
+ *
+ * How to get Pantheon aliases: https://pantheon.io/docs/articles/local/drush-command-line-utility/
+ */
+$aliases['example.dev'] = array(
+  'uri' => 'example.gotpantheon.com',
+  'db-url' => 'mysql://pantheon:02dba546a64c48bfa103347c00a423f56@dbserver.dev.84ce74ad-9cf0-4584-86e2-2e5f16a0febb.drush.in:10376/pantheon',
+  'db-allows-remote' => TRUE,
+  'remote-host' => 'appserver.dev.84ce74ad-9cf0-4593-86e2-2e5f16a0febb.drush.in',
+  'remote-user' => 'dev.84ce74ad-9cf0-4593-86e2-2e5f16a0febb',
+  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+  'path-aliases' => array(
+    '%files' => 'code/sites/default/files',
+    '%drush-script' => 'drush' . $drush_major_version,
+   ),
+);
+
+
+/**
+ * A sample Acquia alias
+ *
+ * How to get Acquia aliases: https://docs.acquia.com/cloud/drush-aliases
+ */
 $aliases['dev'] = array(
   'root' => '/var/www/html/example.dev/docroot',
   'ac-site' => 'example',
   'ac-env' => 'dev',
   'ac-realm' => 'prod',
   'uri' => 'example.prod.acquia-sites.com',
-  'remote-host' => 'staging-8343.prod.hosting.acquia.com',
+  'remote-host' => 'develop-8343.prod.hosting.acquia.com',
   'remote-user' => 'example.dev',
   'path-aliases' => array(
     '%drush-script' => 'drush' . $drush_major_version,
   )
 );
 $aliases['dev.livedev'] = array(
-  'parent' => '@urban.dev',
+  'parent' => '@example.dev',
   'root' => '/mnt/gfs/urban.dev/livedev/docroot',
 );
